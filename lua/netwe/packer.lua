55b1900833a4398cca5af49865628ed0cb54e3f5
IroNEDR <tirasekin@gmail.com>
2024-02-04 21:06:35 +0100

    undotree, harpoon, started lsp

--- lua/netwe/packer.lua
+++ lua/netwe/packer.lua
@@ -12,4 +12,35 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   } 
+  use({ "rebelot/kanagawa.nvim",
+	as = 'kanagawa',
+	config = function()
+		vim.cmd('colorscheme kanagawa')
+	end
+  })
+  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use "nvim-lua/plenary.nvim" 
+  use {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} }
+  }
+  use "mbbill/undotree"
+  use "tpope/vim-fugitive"
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  requires = {
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    -- {'williamboman/mason.nvim'},
+    -- {'williamboman/mason-lspconfig.nvim'},
+
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+  }
+}
 end)

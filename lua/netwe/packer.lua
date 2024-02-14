ed017fea9c711af6ae950ca88cef672c9818d409
IroNEDR <tirasekin@gmail.com>
2024-02-14 01:12:19 +0100

    norg

--- lua/netwe/packer.lua
+++ lua/netwe/packer.lua
@@ -42,5 +42,25 @@ return require('packer').startup(function(use)
     {'hrsh7th/cmp-nvim-lsp'},
     {'L3MON4D3/LuaSnip'},
   }
-}
+  }
+  use { "nvim-neorg/neorg",
+      config = function()
+          require('neorg').setup {
+              load = {
+                  ["core.defaults"] = {}, -- Loads default behaviour
+                  ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.dirman"] = { -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/notes",
+                        },
+                    },
+                },
+            },
+        }
+    end,
+    run = ":Neorg sync-parsers",
+    requires = "nvim-lua/plenary.nvim",
+  }
+
 end)
